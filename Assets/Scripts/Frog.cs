b8b221db6636fa4ba840fffd86cf249b9bbe9fa6
Joao <progjvdallagnol2001@gmail.com>
2025-02-05 10:58:51 -0300

    Adding enemy frog on level_3 and fixing trampoline delay animation

---
+++ Assets/Scripts/Frog.cs
@@ -0,0 +1,48 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Frog : MonoBehaviour {
+    
+    private Rigidbody2D rig;
+    private Animator anim;
+    public float speed;
+    public Transform rightCol;
+    public Transform leftCol;
+    public Transform headPoint;
+    private bool colliding;
+    public LayerMask layer;
+    public BoxCollider2D boxCollider2D;
+    public CircleCollider2D circleCollider2D;
+
+    void Start() {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update() {
+        rig.linearVelocity = new Vector2(speed, rig.linearVelocity.y);
+
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
+
+        if (colliding) {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1f;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Player") {
+            float height = col.contacts[0].point.y - headPoint.position.y;
+
+            if (height > 0) {
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                speed = 0;
+                anim.SetTrigger("Died");
+                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
+                rig.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, 0.33f);
+            }
+        }
+    }
+}
